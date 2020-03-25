eee01cf989233e030d5d7fea268a2ea7dc1c3eca
iCarlo <jcbbasilio28@gmail.com>
2020-03-25 13:17:41 +0800

    Search model created

--- iprocarlo_app/models.py
+++ iprocarlo_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Search(models.Model):
+    search_item = models.CharField(max_length=200)
+    search_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return '{}'.format(self.search_text)
+
+    class Meta:
+        verbose_name_plural = 'Searches'

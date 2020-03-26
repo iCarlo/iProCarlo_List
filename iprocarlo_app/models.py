f066d52a475e8213668d279854709a78eb6908be
iCarlo <jcbbasilio28@gmail.com>
2020-03-26 21:59:15 +0800

    database modified and stable so far

--- iprocarlo_app/models.py
+++ iprocarlo_app/models.py
@@ -1,11 +1,23 @@
+import datetime
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
 
 
 class Search(models.Model):
     search_item = models.CharField(max_length=200)
-    search_date = models.DateTimeField('date published')
+    search_date = models.DateTimeField('date searched')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.search_date <= now
+    was_published_recently.admin_order_field = 'search_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Searched recently?'
+
+    def days_since_searched(self):
+        diff = timezone.now() - self.search_date
+        return diff.days
+    days_since_searched.short_description = 'No. days since searched'
 
     def __str__(self):
         return '{}'.format(self.search_item)
@@ -15,7 +27,7 @@ class Search(models.Model):
 
 
 class Filter(models.Model):
-    searched = models.ForeignKey(Search, on_delete=models.CASCADE)
+    searched = models.ForeignKey(Search, null=True, on_delete=models.CASCADE)
     filter_text = models.CharField(max_length=200)
 
     def __str__(self):
